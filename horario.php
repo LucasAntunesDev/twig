4c9865ab0d2beec25684fecf947f5f8822f70b38
LucasAntunesDev <lucasantuneslara06@gmail.com>
2024-04-04 08:59:51 -0300

    update: página de horários

--- horario.php
+++ horario.php
@@ -2,4 +2,10 @@
 # horario.php
 require('twig_carregar.php');
 
-echo $twig->render('horario.html');
+use Carbon\Carbon;
+
+$data = Carbon::now('America/Sao_Paulo')->locale('pt_BR')->isoFormat('LLLL');
+
+$amanha = Carbon::now('America/Sao_Paulo')->addDay()->locale('pt_BR')->isoFormat('LLLL');
+
+echo $twig->render('horario.html', ['titulo' => 'Horário', 'hoje' => $data, 'amanha' => $amanha]);
